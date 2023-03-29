4dbadab06f8ead614503c9caa0f5e555c0b6bcde
Veronica <veronica.carbonari@gmail.com>
2023-03-29 08:06:23 -0300

    Encapsulamiento
    Agrego getters y setters de los atributos del objeto

--- src/Ejercicio1/Empleado.java
+++ src/Ejercicio1/Empleado.java
@@ -2,13 +2,35 @@ package Ejercicio1;
 
 public class Empleado {
 	
+	//ATRIBUTOS DEL OBJETO
 	private final int id;
 	private String nombre;
 	private int edad;
 
+	//ATRIBUTOS DE LA CLASE
 	private static int contLegajo = 1000;
 	
+	//CONSTRUCTOR
 	public Empleado() {
 		id=contLegajo++;
+	}
+
+	//GETTERS Y SETTERS 
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getId() {
+		return id;
 	}
 }
