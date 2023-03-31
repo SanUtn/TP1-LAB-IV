560f35d9a9e1ba8c2beb650115c118cf9bcd6aa6
SanUtn <105830363+SanUtn@users.noreply.github.com>
2023-03-31 17:58:53 -0300

    update ejercicio 8

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -6,7 +6,11 @@ public class Principal {
 		
 		Empleado [] empleados = new Empleado [5];
 		empleados[0] = new Empleado();
+		empleados[0].setNombre("Josefina");
+		empleados[0].setEdad(18);
 		empleados[1] = new Empleado();
+		empleados[1].setNombre("Carlos");
+		empleados[1].setEdad(52);
 		empleados[2] = new Empleado("Juan", 30);
 		empleados[3] = new Empleado("Sara", 20);
 		empleados[4] = new Empleado("Pedro", 45);
